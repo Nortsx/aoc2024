256cbf1224196d3ddc04fde410c98db31f4140f5
nort <vsevolodsevostyanov3@gmail.com>
2025-04-24 13:01:16 +0200

    day 19 part 1

---
+++ 19/main.py
@@ -0,0 +1,22 @@
+def check_towel(towels_data, pattern_data):
+    if pattern_data == "":
+        return True
+    for i in range(len(towels_data)):
+        if len(towels_data[i]) > len(pattern_data):
+            continue
+        if towels_data[i] == pattern_data[0:len(towels[i])]:
+            if check_towel(towels_data, pattern_data[len(towels[i]):]):
+                return True
+    return False
+
+
+data = [line.strip() for line in open("input.txt").readlines()]
+towels = [towel.strip() for towel in data[0].split(',')]
+patterns = data[2:]
+
+counter = 0
+for pattern in patterns:
+    if check_towel(towels, pattern):
+        counter += 1
+
+print(counter)
